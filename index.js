b5680446baf7e004357bc53a60a8f838def8bc85
herlambang0321 <herlambang88888888@gmail.com>
2021-08-12 13:15:57 +0700

    new revisi challenge20 done

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const dbFile = __dirname + "/db/data.db";
 
 let db = new sqlite3.Database(path.join(__dirname, "data.db"), (err) => {
   if (err) throw err;
-  //   if (err) {
-  //     return console.error(err.message);
-  //   }
-  //   console.log("Connected to SQLite database.");
 });
-
-// const users = []
 
 app.set("views", path.join(__dirname, "views")); // specify the views
 app.set("view engine", "ejs");
@@ -27,23 +21,71 @@ app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
+  const { id, string, integer, float, date, boolean } = req.query;
+
+  const url = req.url == "/" ? "/?page=1" : req.url;
   const page = req.query.page || 1;
   const limit = 3;
+  const offset = (page - 1) * limit;
 
-  db.all("SELECT COUNT(*) as total FROM newData", (err, data) => {
+  let params = [];
+
+  if (id) {
+    params.push(`id=${id}`);
+  }
+
+  if (string) {
+    params.push(`string like '%${string}%'`);
+  }
+
+  if (integer) {
+    params.push(`integer=${integer}`);
+  }
+
+  if (float) {
+    params.push(`float=${float}`);
+  }
+
+  if (date) {
+    params.push(`date=${date}`);
+  }
+
+  if (boolean) {
+    params.push(`boolean='${boolean}'`);
+  }
+
+  let sql = "SELECT COUNT(*) as total FROM newData";
+
+  if (params.length > 0) {
+    sql += ` WHERE ${params.join(" and ")}`;
+  }
+  // console.log(sql)
+  db.all(sql, (err, data) => {
     if (err) return res.send(err);
 
     const total = data[0].total;
-    const offset = (page - 1) * limit;
     const pages = Math.ceil(total / limit);
 
-    db.all(
-      `SELECT * FROM newData limit ${limit} offset ${offset}`, (err, data) => {
-        if (err) return res.send(err);
+    sql = `SELECT * FROM newData`;
 
-        res.render("index", { data: data, page, pages, offset });
-      }
-    );
+    if (params.length > 0) {
+      sql += ` WHERE ${params.join(" and ")}`;
+    }
+
+    sql += ` limit ${limit} offset ${offset}`;
+
+    db.all(sql, (err, data) => {
+      if (err) return res.send(err);
+
+      res.render("index", {
+        data: data,
+        page,
+        pages,
+        offset,
+        url,
+        query: req.query,
+      });
+    });
   });
 });
 
@@ -58,7 +100,8 @@ app.get("/add", (req, res) => res.render("add"));
 app.post("/add", (req, res) => {
   const { string, integer, float, date, boolean } = req.body;
   db.run(
-    `INSERT INTO newData (string, integer, float, date, boolean) VALUES ('${string}', ${integer}, ${float}, '${date}', '${boolean}')`, (err, data) => {
+    `INSERT INTO newData (string, integer, float, date, boolean) VALUES ('${string}', ${integer}, ${float}, '${date}', '${boolean}')`,
+    (err, data) => {
       if (err) throw err;
       res.redirect("/");
     }
